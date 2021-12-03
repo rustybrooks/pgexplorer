987d50a7527f51c8f96ee81175ea9fe0231f4d4b
Rusty Brooks <rbrooks@alienvault.com>
2021-12-03 15:08:03 -0600

    Fix interp, add script again

---
+++ src/bin/index.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import dotenv from 'dotenv';
+import chalk from 'chalk';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+import * as db from '../db';
+
+async function main() {
+  // const res = await db.tableConstraints();
+  const res = await db.tableConstraintDeleteOrder({});
+  console.table(res);
+}
+
+const yarg = yargs(hideBin(process.argv));
+
+yarg.usage('Usage: <subcommand>');
+yarg.wrap(Math.min(130, yarg.terminalWidth()));
+
+yarg.option('env', {
+  describe: 'name of env file to use for database creds',
+  default: '.env.local',
+  type: 'string',
+});
+
+// eslint-disable-next-line @typescript-eslint/dot-notation
+dotenv.config({ path: yarg.argv['env'] });
+
+db.setupDb();
+main().then(() => process.exit(0));
